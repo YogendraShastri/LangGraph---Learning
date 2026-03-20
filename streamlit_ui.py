b83cd1d8b21272e3f78d37735ff76fc6a5a542c4
Yogendra Shastri <shastri.yogendra32@gmail.com>
2026-03-20 21:30:39 +0530

    stream lit chatbot

--- streamlit_ui.py
+++ streamlit_ui.py
@@ -17,18 +17,43 @@ for message in st.session_state['message_history']:
 
 user_input = st.chat_input('Type here')
 
-if user_input:
+# if user_input:
 
+#     # first add the message to message_history
+#     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+#     with st.chat_message('user'):
+#         st.text(user_input)
+
+#     # chatbot
+#     response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+#     ai_message = response['messages'][-1].content
+
+#     # add to user history
+#     st.session_state['message_history'].append({'role': 'assistant', 'content' : ai_message})
+#     with st.chat_message('assistant'):
+#         st.text(ai_message)
+
+
+
+# adding streaming or typing effect using streamlit
+
+    # first add the message to message_history
+
+if user_input:
     # first add the message to message_history
     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
 
-    # chatbot
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
-    ai_message = response['messages'][-1].content
-
-    # add to user history
-    st.session_state['message_history'].append({'role': 'assistant', 'content' : ai_message})
+    # first add the message to message_history
     with st.chat_message('assistant'):
-        st.text(ai_message)
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode= 'messages'
+            )
+        )
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
